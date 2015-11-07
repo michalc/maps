8ab13a5eeff9935c487f02282e2f974c50ef4f06
Michal Charemza <michalcharemza@gmail.com>
2015-11-07 13:20:38 +0000

    Start of using NOAA data for charts

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Deployed to [http://maps.charemza.name/](http://maps.charemza.name/)
 
+The contents of the `data` directory are derived from NOAA National Geophysical Data Center http://www.ngdc.noaa.gov/mgg/shorelines/shorelines.html. and is released under the LGPL. The remainder of this project is released under the MIT License.
+
 Chart provided by [amcharts.com](http://www.amcharts.com/) under [CC BY-NC 4.0](http://creativecommons.org/licenses/by-nc/4.0/)
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,58 @@ var eslint = require('gulp-eslint');
 var changed = require('gulp-changed');
 var merge = require('merge-stream');
 
+// Requires dev dependencies to be installed
+gulp.task('download-charts', function () {
+  var download = require('gulp-download');
+  var unzip = require('gulp-unzip');
+  var url = 'http://www.ngdc.noaa.gov/mgg/shorelines/data/gshhg/latest/gshhg-shp-2.3.4.zip';
+  return download(url)
+    .pipe(unzip())
+    .pipe(gulp.dest('data_src/'));
+});
+
+// Requires dev dependencies to be installed,
+// and download-charts task to be run
+gulp.task('generate-charts', function () {
+  var geojsonStream = require('geojson-stream');
+  var mapnik = require('mapnik');
+  var fs = require('fs');
+  var promise = require('promise');
+  var path = require('path');
+
+  mapnik.register_default_input_plugins();
+
+  // Can't seem to find way for mapnik to handle files asynchronously
+  var root = 'data_src/WDBII_shp/c/'
+  var inputs = [
+    root + 'WDBII_border_c_L1.shp',
+    root + 'WDBII_border_c_L2.shp',
+    root + 'WDBII_border_c_L3.shp'
+  ];
+  var outputDir = 'data/';
+
+  inputs.forEach(function(input) {
+    var output = outputDir + path.basename(input, '.shp') + '.json';
+
+    var fileOut = fs.createWriteStream(output);
+    var geojsonOut = geojsonStream.stringify();
+    geojsonOut.pipe(fileOut);
+
+    var ds = new mapnik.Datasource({type:'shape', file: input});
+    var featureset = ds.featureset()
+    var feat = featureset.next();
+
+    while (feat) {
+      geojsonOut.write(JSON.parse(feat.toJSON()));
+      feat = featureset.next();
+    }
+
+    geojsonOut.end();
+  });
+
+  return promise.resolve();
+});
+
 gulp.task('lint', function () {
   return gulp.src(['gulpfile.js', 'src/*.js'])
     .pipe(eslint())
@@ -14,9 +66,15 @@ gulp.task('lint', function () {
 
 gulp.task('default', [], function() {
   var build = 'build';
+  var dest = build;
+
+  var dataSrc = 'data/*.json';
+  var dataDest = build + '/data';
+  var dataFiles = gulp.src(dataSrc)
+    .pipe(changed(dataDest))
+    .pipe(gulp.dest(dataDest));
 
   var src = 'src/*';
-  var dest = build;
   var srcFiles = gulp.src(src)
     .pipe(changed(dest))
     .pipe(gulp.dest(dest));
@@ -33,5 +91,5 @@ gulp.task('default', [], function() {
     .pipe(changed(bowerDest))
     .pipe(gulp.dest(bowerDest));
 
-  return merge(srcFiles, bowerFiles);
+  return merge(dataFiles, srcFiles, bowerFiles);
 });
